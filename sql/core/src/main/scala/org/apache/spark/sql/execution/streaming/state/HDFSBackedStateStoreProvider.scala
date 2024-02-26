bc08190f68b420b4d9193e752b50568f7487df09
Eric Marnadi <eric.marnadi@databricks.com>
2024-02-26 11:34:04 -0800

    Background Thread approach works

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/HDFSBackedStateStoreProvider.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/HDFSBackedStateStoreProvider.scala
@@ -125,11 +125,7 @@ private[sql] class HDFSBackedStateStoreProvider extends StateStoreProvider with 
         valueSchema: StructType,
         useMultipleValuesPerKey: Boolean = false,
         isInternal: Boolean = false): Unit = {
-      if (colFamilyName == "ttl") {
-        throw StateStoreErrors.ttlNotSupportedWithProvider("HDFSStateStoreProvider")
-      } else {
-        throw StateStoreErrors.multipleColumnFamiliesNotSupported("HDFSStateStoreProvider")
-      }
+      throw StateStoreErrors.multipleColumnFamiliesNotSupported("HDFSStateStoreProvider")
     }
 
     override def get(key: UnsafeRow, colFamilyName: String): UnsafeRow = {
