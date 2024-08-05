df5c451b54cb3d3bd47f6fc9362de20b8160ba22
Eric Marnadi <eric.marnadi@databricks.com>
2024-08-05 09:39:03 -0700

    tests should pass

--- sql/core/src/test/scala/org/apache/spark/sql/streaming/TransformWithStateSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/streaming/TransformWithStateSuite.scala
@@ -902,7 +902,7 @@ class TransformWithStateSuite extends StateStoreMetricsTest
           new StructType().add("value", LongType, false),
           Some(NoPrefixKeyStateEncoderSpec(keySchema)),
           None,
-          0.toShort
+          1.toShort
         )
         val schema1 = StateStoreColFamilySchema(
           "listState",
@@ -912,7 +912,7 @@ class TransformWithStateSuite extends StateStoreMetricsTest
               .add("name", StringType),
           Some(NoPrefixKeyStateEncoderSpec(keySchema)),
           None,
-          1.toShort
+          2.toShort
         )
 
         val userKeySchema = new StructType()
@@ -927,7 +927,7 @@ class TransformWithStateSuite extends StateStoreMetricsTest
           new StructType().add("value", StringType),
           Some(PrefixKeyScanStateEncoderSpec(compositeKeySchema, 1)),
           Option(userKeySchema),
-          2.toShort
+          3.toShort
         )
 
         val inputData = MemoryStream[String]
